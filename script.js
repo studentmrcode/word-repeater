4c9381309b8008fdca0b15dc1f3085627bec9810
David <davidk@mrcodeswildride.com>
2018-11-15 17:02:43 +0000

    init

---
+++ script.js
@@ -0,0 +1,9 @@
+// Make variables for the text box, button, and paragraph.
+// You can name them anything you want, but for simplicity, name them the same as the IDs.
+
+
+// Add an event listener to the button to show the story.
+// You can name the function anything you want, but pick something that indicates action, like showStory.
+
+
+// Make a function to show the story.
