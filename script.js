f9264b181259516ab7af03e4feeb0d54be34335b
David <davidk@mrcodeswildride.com>
2018-11-20 21:43:31 +0000

    init

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// Section 1: Make separate variables for the text box, buttons, and paragraphs.
+// Section 1: Make separate variables for the text box, number box, button, and paragraph.
 // You can name them anything you want, but for simplicity, name them the same as the IDs.
 
 
-// Section 2: Make an empty array to store the submitted names.
+// Section 2: Add an event listener to the button to repeat the word the specified number of times.
+// You can name the function anything you want, but pick something that indicates action.
 
 
-// Section 3: Add separate event listeners to the buttons.
-// You can name the functions anything you want, but pick something that indicates action.
-
-
-// Section 4:  Make separate functions to submit a name and pick a winner.
+// Section 3: Make a function to repeat the word the specified number of times.
