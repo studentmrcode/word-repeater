c120eb828f562cf4cfe4875f9fc2b7171c7cbf2d
David <davidk@mrcodeswildride.com>
2018-11-20 20:49:54 +0000

    init

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-// Section 1: Make separate variables for the text box, button, and paragraph.
+// Section 1: Make separate variables for the text box, buttons, and paragraphs.
 // You can name them anything you want, but for simplicity, name them the same as the IDs.
 
 
-// Section 2: Add an event listener to the button to show the story.
-// You can name the function anything you want, but pick something that indicates action, like showStory.
+// Section 2: Make an empty array to store the submitted names.
 
 
-// Section 3: Make a function to show the story.
+// Section 3: Add separate event listeners to the buttons.
+// You can name the functions anything you want, but pick something that indicates action.
+
+
+// Section 4:  Make separate functions to submit a name and pick a winner.
