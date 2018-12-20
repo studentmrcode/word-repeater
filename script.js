e070cd7ef882d340bfd9a55b63638785198afcbc
David <davidk@mrcodeswildride.com>
2018-12-20 02:11:13 +0000

    removed CSS and JS comments

--- script.js
+++
@@ -1,9 +0,0 @@
-// Section 1: Make separate variables for the text box, number box, button, and paragraph.
-// You can name them anything you want, but for simplicity, name them the same as the IDs.
-
-
-// Section 2: Add an event listener to the button to repeat the word the specified number of times.
-// You can name the function anything you want, but pick something that indicates action.
-
-
-// Section 3: Make a function to repeat the word the specified number of times.
